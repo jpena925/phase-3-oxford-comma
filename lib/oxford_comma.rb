4cd9a4055abbd7ead081d59f09d9deebf1773e67
Jack Pena <penajackc@gmail.com>
2022-02-22 10:45:29 -0700

    commit tree 44b321579668c3017b81af15339da002a6d5fa2a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
-def oxford_comma(array)
+require 'pry'
 
-end
+# def oxford_comma(array)
+#     if array.length == 1
+#         array.join
+#     elsif array.length == 2
+#         final_item = array.pop
+#         "#{array.join} and #{final_item}"
+#     else
+#         final_item = array.pop
+#         binding.pry
+#         partial_string = array.join(', ')
+#         return "#{partial_string}, and #{final_item}"
+#     end
+# end
+
+def oxford_comma(array)
+    return array.join(" and ") if array.size < 3
+  
+    # insert 'and ' in front of the last string in the array
+    array[-1] = "and #{array[-1]}"
+  
+    array.join(", ")  
+  end
+
